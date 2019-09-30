35672a1e7864edbd062275e1affc0553fcb1b407
Johann <johanndeveloper@google.com>
2019-09-30 11:02:49 -0400

    React Todo App: Phase 5 - Remove no OnChange Error for checkboxes

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,10 +1,14 @@
 import React from "react"
 
 function ToDoItem(props){
-    console.log(props.item.text)
+    // console.log(props.item.text)
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={props.item.completed} ></input>
+            <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={() => alert("Changed...in theory")}
+                ></input>
             <p>{props.item.text}</p>
         </div>
     )
